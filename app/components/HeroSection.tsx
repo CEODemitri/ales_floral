73e4f63ceb3d48be76d4bae4412f2bc943adfe18
CEODemitri <demichole@gmail.com>
2023-12-19 14:22:53 -0600

    i wonder will the folder name change too

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+'use client'
+  
+export default function HeroSection() {
+   return (
+    <div className="w-full h-96 bg-white">
+      <h1>
+        Hallo, ich bin Demitri. 
+        <p></p>
+      </h1>
+    </div>
+  )
+}
